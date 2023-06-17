c8138efb0e7af4d83d8e87d0418769424076965c
Marc Arnold <m_o_arnold@gmx.de>
2023-06-17 21:02:25 +0200

    Implements \isImmutableField for Terms, uses \not \isImmutableField for normal read and write rules

--- key.core/src/main/java/de/uka/ilkd/key/rule/conditions/ImmutableFieldCondition.java
+++ key.core/src/main/java/de/uka/ilkd/key/rule/conditions/ImmutableFieldCondition.java
@@ -1,9 +1,13 @@
 package de.uka.ilkd.key.rule.conditions;
 
 import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.TermImpl;
 import de.uka.ilkd.key.logic.op.*;
+import de.uka.ilkd.key.logic.sort.Sort;
 import de.uka.ilkd.key.rule.VariableConditionAdapter;
 import de.uka.ilkd.key.rule.inst.SVInstantiations;
+import de.uka.ilkd.key.staticanalysis.OpalResultProvider;
 
 public class ImmutableFieldCondition extends VariableConditionAdapter {
 
@@ -22,10 +26,22 @@ public class ImmutableFieldCondition extends VariableConditionAdapter {
     public boolean check(SchemaVariable var, SVSubstitute instCandidate, SVInstantiations instMap,
                          Services services) {
         final Object o = instMap.getInstantiation(field);
-        if (!(o instanceof LocationVariable)) {
-            return false;
+        if (o instanceof LocationVariable) {
+            return (((LocationVariable)o).isImmutable()) != negated;
         }
-        return ((LocationVariable)o).isImmutable();
+
+        if (o instanceof Term) {
+            Operator op = ((Term) o).op();
+            if (op instanceof Function) {
+                String[] splitted = op.name().toString().split("::[$]");
+                if (splitted.length < 2) {
+                    return false;
+                }
+                ProgramVariable attribute = services.getJavaInfo().getAttribute(splitted[1], splitted[0]);
+                return attribute.isImmutable() != negated;
+            }
+        }
+        return false;
     }
 
     public String toString() {
