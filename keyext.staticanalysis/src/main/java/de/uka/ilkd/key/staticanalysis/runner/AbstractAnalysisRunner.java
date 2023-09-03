1acc9de1cbf31473a2ea830689d0064448883ae0
Marc Arnold <m_o_arnold@gmx.de>
2023-09-03 15:09:13 +0200

    Adds function to set domain for AI

--- keyext.staticanalysis/src/main/java/de/uka/ilkd/key/staticanalysis/runner/AbstractAnalysisRunner.java
+++ keyext.staticanalysis/src/main/java/de/uka/ilkd/key/staticanalysis/runner/AbstractAnalysisRunner.java
@@ -4,6 +4,8 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigValueFactory;
 import de.uka.ilkd.key.staticanalysis.StaticAnalysisSettings;
+import org.opalj.ai.domain.l2.DefaultPerformInvocationsDomainWithCFGAndDefUse;
+import org.opalj.ai.fpcf.properties.AIDomainFactoryKey$;
 import org.opalj.br.analyses.Project;
 import org.opalj.br.analyses.cg.AllEntryPointsFinder$;
 import org.opalj.br.analyses.cg.InitialEntryPointsKey$;
@@ -13,10 +15,16 @@ import org.opalj.fpcf.ComputationSpecification;
 import org.opalj.fpcf.PropertyStore;
 import org.opalj.log.GlobalLogContext$;
 import org.opalj.tac.cg.RTACallGraphKey$;
+import scala.Function1;
+import scala.Option;
 import scala.collection.immutable.Seq;
+import scala.collection.immutable.Set;
+import scala.jdk.javaapi.CollectionConverters;
+import scala.runtime.AbstractFunction1;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
 
 public abstract class AbstractAnalysisRunner {
 
@@ -42,7 +50,18 @@ public abstract class AbstractAnalysisRunner {
 
     abstract void evaluateResult();
 
+    void setDomain() {
+//        Function1<Option<Set<Class<?>>>, Set<Class<?>>> infoFunction = new AbstractFunction1() {
+//            @Override
+//            public Object apply(Object v1) {
+//                return CollectionConverters.asScala(Arrays.asList(DefaultPerformInvocationsDomainWithCFGAndDefUse.class)).toSet();
+//            }
+//        };
+//        p.updateProjectInformationKeyInitializationData(AIDomainFactoryKey$.MODULE$, infoFunction);
+    }
+
     private void startAnalysis() {
+        setDomain();
         p.get(RTACallGraphKey$.MODULE$);
         store = p.get(FPCFAnalysesManagerKey$.MODULE$).runAll(
                 determineAnalyses()
