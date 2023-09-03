6f736e1450c0453a3332fa504f968ef371889ffb
Marc Arnold <m_o_arnold@gmx.de>
2023-09-03 18:16:02 +0200

    Adds possibility do analize the jdk files

--- keyext.staticanalysis/src/main/java/de/uka/ilkd/key/staticanalysis/runner/AbstractAnalysisRunner.java
+++ keyext.staticanalysis/src/main/java/de/uka/ilkd/key/staticanalysis/runner/AbstractAnalysisRunner.java
@@ -4,13 +4,16 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigValueFactory;
 import de.uka.ilkd.key.staticanalysis.StaticAnalysisSettings;
+import org.apache.commons.lang3.NotImplementedException;
 import org.opalj.ai.domain.l2.DefaultPerformInvocationsDomainWithCFGAndDefUse;
 import org.opalj.ai.fpcf.properties.AIDomainFactoryKey$;
 import org.opalj.br.analyses.Project;
 import org.opalj.br.analyses.cg.AllEntryPointsFinder$;
 import org.opalj.br.analyses.cg.InitialEntryPointsKey$;
+import org.opalj.br.fpcf.FPCFAnalysesManager;
 import org.opalj.br.fpcf.FPCFAnalysesManagerKey$;
 import org.opalj.br.fpcf.FPCFAnalysis;
+import org.opalj.br.fpcf.PropertyStoreKey$;
 import org.opalj.fpcf.ComputationSpecification;
 import org.opalj.fpcf.PropertyStore;
 import org.opalj.log.GlobalLogContext$;
@@ -40,6 +43,12 @@ public abstract class AbstractAnalysisRunner {
             config = config.withValue("org.opalj.br.analyses.cg.ClassExtensibilityKey.analysis",
                     ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ClassHierarchyIsNotExtensible"));
         }
+
+        // With JDK
+//        File[] classfiles = new File[] {new File(pathToJar)};
+//        File[] libFiles = new File[] {getJREFolder()};
+//        p = Project.apply(classfiles, libFiles, GlobalLogContext$.MODULE$, config);
+        // Without JDK
         p = Project.apply(new File(pathToJar), GlobalLogContext$.MODULE$, config);
     }
 
@@ -62,14 +71,54 @@ public abstract class AbstractAnalysisRunner {
 
     private void startAnalysis() {
         setDomain();
+        store = p.get(PropertyStoreKey$.MODULE$);
+        FPCFAnalysesManager manager = p.get(FPCFAnalysesManagerKey$.MODULE$);
         p.get(RTACallGraphKey$.MODULE$);
-        store = p.get(FPCFAnalysesManagerKey$.MODULE$).runAll(
+        store = manager.runAll(
                 determineAnalyses()
         )._1;
         store.waitOnPhaseCompletion();
-    };
+    }
 
     abstract Seq<ComputationSpecification<FPCFAnalysis>> determineAnalyses();
+
+
+    private File getJREFolder() {
+
+        String javaVersion = System.getProperty("java.version");
+        if (javaVersion.startsWith("1.")) {
+//            String s
+//            unBootClassPath = System.getProperties().getProperty("sun.boot.class.path");
+//            String[] paths = sunBootClassPath.split(File.pathSeparator);
+//            for (String s: paths) {
+//                if (s.endsWith("rt.jar")) {
+//
+//                }
+//            }
+//
+//            paths.find(_.endsWith("rt.jar")) match {
+//
+//                case Some(libPath) =>
+//                    new File(libPath.substring(0, libPath.length() - 6))
+//
+//                case None =>
+//                    val sunBootLibraryPath = System.getProperty("sun.boot.library.path")
+//                    if (sunBootLibraryPath == null) {
+//                        throw new RuntimeException("cannot locate the JRE libraries")
+//                    } else {
+//                        new File(sunBootLibraryPath)
+//                    }
+//            }
+            throw new NotImplementedException("TODO!");
+        } else {
+            String javaJMods = System.getProperty("java.home") + "/jmods";
+            File directory = new File(javaJMods);
+            if (!directory.exists()) {
+                throw new RuntimeException("cannot locate the JRE libraries");
+            }
+            return directory;
+        }
+    }
 }
 
 
