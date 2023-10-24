44d9104ee5fe14c6a365cef3286127f4c4eb3965
Marc Arnold <m_o_arnold@gmx.de>
2023-10-24 17:36:59 +0200

    Adds preparations for benchmarking using the key example files

--- key.core/src/main/java/de/uka/ilkd/key/opal/StaticAnalysisSettings.java
+++ key.core/src/main/java/de/uka/ilkd/key/opal/StaticAnalysisSettings.java
@@ -3,21 +3,21 @@ package de.uka.ilkd.key.opal;
 import de.uka.ilkd.key.opal.runner.AnalysisLevel;
 
 public class StaticAnalysisSettings {
-    public static boolean useFieldImmutabilityAnalysis = Boolean.valueOf(System.getProperty("useFieldImmutabilityAnalysis", "true"));
+    public static boolean useFieldImmutabilityAnalysis = Boolean.valueOf(System.getProperty("useFieldImmutabilityAnalysis", "false"));
     public static AnalysisLevel fieldImmutabilityLevel = AnalysisLevel.valueOf(System.getProperty("fieldImmutabilityLevel", "L1"));
-    public static boolean useMethodPurityAnalysis = Boolean.valueOf(System.getProperty("useMethodPurityAnalysis", "true"));
+    public static boolean useMethodPurityAnalysis = Boolean.valueOf(System.getProperty("useMethodPurityAnalysis", "false"));
     public static AnalysisLevel methodPurityLevel = AnalysisLevel.valueOf(System.getProperty("methodPurityLevel", "L1"));
 
     public static boolean useCloseWorldAssumption = Boolean.valueOf(System.getProperty("useCloseWorldAssumption", "false"));
     public static boolean analyzeJDKFiles = Boolean.valueOf(System.getProperty("analyzeJDKFiles", "true"));
 
     // Field Immutability Analysis Usescases:
-    private static boolean useRevisedHeapTheory = Boolean.valueOf(System.getProperty("useRevisedHeapTheory", "true"));
+    private static boolean useRevisedHeapTheory = Boolean.valueOf(System.getProperty("useRevisedHeapTheory", "false"));
 
     //Method Purity Analysis Usecases:
-    private static boolean useAssignableClauseGeneration = Boolean.valueOf(System.getProperty("useAssignableClauseGeneration", "true"));
-    private static boolean useHeapParameterRemoval = Boolean.valueOf(System.getProperty("useRemovalHeapParameter", "true"));
-    private static boolean useAssignableClauseReduction = Boolean.valueOf(System.getProperty("useReductionOfAssignable", "true"));
+    private static boolean useAssignableClauseGeneration = Boolean.valueOf(System.getProperty("useAssignableClauseGeneration", "false"));
+    private static boolean useHeapParameterRemoval = Boolean.valueOf(System.getProperty("useHeapParameterRemoval", "false"));
+    private static boolean useAssignableClauseReduction = Boolean.valueOf(System.getProperty("useAssignableClauseReduction", "false"));
 
     public static boolean anyAnalysisSelected() {
         return useFieldImmutabilityAnalysis || useMethodPurityAnalysis; // | ... Add new analyses here...
