b825228058d539f98b154e53e808c034c2824f09
Marc Arnold <m_o_arnold@gmx.de>
2023-12-21 10:24:42 +0100

    Uses alternative solution to configure static analyses for benchmarking

--- key.core/src/main/java/de/uka/ilkd/key/opal/StaticAnalysisSettings.java
+++ key.core/src/main/java/de/uka/ilkd/key/opal/StaticAnalysisSettings.java
@@ -2,23 +2,53 @@ package de.uka.ilkd.key.opal;
 
 import de.uka.ilkd.key.opal.runner.AnalysisLevel;
 
-public class StaticAnalysisSettings {
-    public static boolean useFieldImmutabilityAnalysis = Boolean.valueOf(System.getProperty("useFieldImmutabilityAnalysis", "false"));
-    public static AnalysisLevel fieldImmutabilityLevel = AnalysisLevel.valueOf(System.getProperty("fieldImmutabilityLevel", "L1"));
-    public static boolean useMethodPurityAnalysis = Boolean.valueOf(System.getProperty("useMethodPurityAnalysis", "false"));
-    public static boolean useExceptionUsageAnalysis = Boolean.valueOf(System.getProperty("useExceptionUsageAnalysis", "false"));
-    public static AnalysisLevel methodPurityLevel = AnalysisLevel.valueOf(System.getProperty("methodPurityLevel", "L1"));
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
 
-    public static boolean useCloseWorldAssumption = Boolean.valueOf(System.getProperty("useCloseWorldAssumption", "false"));
-    public static boolean analyzeJDKFiles = Boolean.valueOf(System.getProperty("analyzeJDKFiles", "true"));
+public class StaticAnalysisSettings {
+
+    public static final String PROPERTY_PATH = "/home/marc/coding/KeYGithubFork/staticAnalysesConfig.properties";
+
+    public static boolean useFieldImmutabilityAnalysis = Boolean.valueOf(readFromPropertyFiles("useFieldImmutabilityAnalysis", "false"));
+    public static AnalysisLevel fieldImmutabilityLevel = AnalysisLevel.valueOf(readFromPropertyFiles("fieldImmutabilityLevel", "L1"));
+    public static boolean useMethodPurityAnalysis = Boolean.valueOf(readFromPropertyFiles("useMethodPurityAnalysis", "false"));
+    public static boolean useExceptionUsageAnalysis = Boolean.valueOf(readFromPropertyFiles("useExceptionUsageAnalysis", "false"));
+    public static AnalysisLevel methodPurityLevel = AnalysisLevel.valueOf(readFromPropertyFiles("methodPurityLevel", "L1"));
+
+    public static boolean useCloseWorldAssumption = Boolean.valueOf(readFromPropertyFiles("useCloseWorldAssumption", "false"));
+    public static boolean analyzeJDKFiles = Boolean.valueOf(readFromPropertyFiles("analyzeJDKFiles", "true"));
 
     // Field Immutability Analysis Usescases:
-    private static boolean useRevisedHeapTheory = Boolean.valueOf(System.getProperty("useRevisedHeapTheory", "false"));
+    private static boolean useRevisedHeapTheory = Boolean.valueOf(readFromPropertyFiles("useRevisedHeapTheory", "false"));
 
     //Method Purity Analysis Usecases:
-    private static boolean useAssignableClauseGeneration = Boolean.valueOf(System.getProperty("useAssignableClauseGeneration", "false"));
-    private static boolean useHeapParameterRemoval = Boolean.valueOf(System.getProperty("useHeapParameterRemoval", "false"));
-    private static boolean useAssignableClauseReduction = Boolean.valueOf(System.getProperty("useAssignableClauseReduction", "false"));
+    private static boolean useAssignableClauseGeneration = Boolean.valueOf(readFromPropertyFiles("useAssignableClauseGeneration", "false"));
+    private static boolean useHeapParameterRemoval = Boolean.valueOf(readFromPropertyFiles("useHeapParameterRemoval", "false"));
+    private static boolean useAssignableClauseReduction = Boolean.valueOf(readFromPropertyFiles("useAssignableClauseReduction", "false"));
+
+    private static String readFromPropertyFiles(String propKeY, String defaultValue) {
+        try {
+            Properties appProps = new Properties();
+            appProps.load(new FileInputStream(PROPERTY_PATH));
+            String val = (String) appProps.get(propKeY);
+            if (val == null) {
+                System.out.println("Property " + propKeY + " not found within config, default value is used.");
+                return defaultValue;
+            }
+            return val;
+        }
+        catch (FileNotFoundException e) {
+            // No output, usually occurs outside of benchmarking (No Property file is used in normal KeY usage).
+            return defaultValue;
+        }
+        catch (Exception e) {
+            System.out.println("Error while reading property from file: " + e.toString());
+            System.out.println("Default value is used.");
+            return defaultValue;
+        }
+    }
+
 
     public static boolean anyAnalysisSelected() {
         return useFieldImmutabilityAnalysis || useMethodPurityAnalysis || useExceptionUsageAnalysis; // | ... Add new analyses here...
